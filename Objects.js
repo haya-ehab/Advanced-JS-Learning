f465a935afa0fc665a8a82c53cbe7bf1b1ae6150
Haya <haya.ehab.dev@gmail.com>
2025-07-06 16:48:48 +0300

    Added New Lessons to Objects.js

--- Objects.js
+++ Objects.js
@@ -47,3 +47,81 @@
 
 // Note: Using '', "", `` are better
 
+// Functions are Objects
+
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+// const Circle1 = new Function('radius', `
+//     this.radius = radius;
+//     this.draw = function() {
+//     console.log('draw);
+//     }
+//     `);
+
+//     const circle = new Circle1(1);
+
+// const another = new Circle(1);
+
+
+// Value vs Reference Types
+
+// Value Types -> 
+// Number, String, Boolean, Symbol, Undefined And Null
+
+// Reference Types ->
+// Objects, Functions, Arrays
+
+// let number = 10;
+
+// function increase(number) {
+//     number++;
+// }
+
+// increase(number);
+// console.log(number);
+
+// Adding or Removing Properties
+
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+// const circle = new Circle(10);
+// const propertyName = 'center location';
+// circle.location = { x: 1 };
+
+// // user,token = 'asdasdasd'
+
+// delete circle['location'];
+
+// Enumerating Properties
+
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+// const circle = new Circle(10);
+
+// for (let key in circle) {
+//     if (typeof circle[key] !== 'function')
+//     console.log(key, circle[key]);
+// }
+
+// const keys = Object.keys(circle);
+// console.log(keys);
+
+// if ('radius' in circle)
+//     console.log('Circle has a radius');
+
+// Abstraction
