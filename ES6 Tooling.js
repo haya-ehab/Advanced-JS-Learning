df2feaba3419064889fa09c678425baf8c6bd4b8
Haya <haya.ehab.dev@gmail.com>
2025-07-11 05:12:57 +0300

    Added Final Lessons on ES6 Tooling

---
+++ ES6 Tooling.js
@@ -0,0 +1,48 @@
+// Modules
+// Why Modularity?
+//-Maintainability
+//-Reuse
+//-Abstract Code
+
+// const _radius = new WeakMap();
+// class Circle {
+//     constructor(radius) {
+//         _radius.set(this, radius);
+//     }
+
+// draw() {
+//     console.log('Circle with radius ' + _radius.get(this));
+// }
+// }
+
+// const c = new Circle(10);
+// c.draw(); 
+// Create  a File and name it Circle.js to export it
+// module.exports = Circle;
+
+// ES6 Modules
+
+// const _radius = new WeakMap();
+
+// class Circle {
+//     constructor(radius) {
+//         _radius.set(this, radius);
+//     }
+
+//     draw() {
+//         console.log('Circle with radius ' + _radius.get(this));
+//     }
+// }
+
+// بصراحه انا جيت عند هنا و ابتديت اكسل اكتب الكود :D
+
+// ES6 Tooling
+// you need Transpiler - Bundler
+// Unless you use Node.js
+// so just install npm :p and webpack the latest version
+
+
+// The End
+
+
+
